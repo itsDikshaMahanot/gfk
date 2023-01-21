93dd302bb4f28dda057789dc5b25c4f020e119e6
itsDikshaMahanot <jainmani786@gmail.com>
2023-01-22 00:59:20 +0530

    finall chnges to tailwind success fully added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
-      <div className="bg-red-500">hello</div>
-      <p className="bg-slate-800">its para</p>
+      <Navbar />
     </>
   );
 }
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <div className="text-lg bg-slate-400 h-16 align-middle self-center">
+        NAVBAR
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
