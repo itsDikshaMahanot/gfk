acd59e338f18b2de98484a80e3f1dc9be5c21382
itsDikshaMahanot <jainmani786@gmail.com>
2023-01-22 00:41:34 +0530

    nnn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
-import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <>
-      <Navbar />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <p>welcome</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
